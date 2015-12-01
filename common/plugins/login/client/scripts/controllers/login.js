ef85d4153c52e4514d3a2a414a58620834d9f17d
robinskumar73 <robinskumar73@gmail.com>
2015-12-01 18:23:05 +0530

    In the middle of bug fixing of login plugin

--- common/plugins/login/client/scripts/controllers/login.js
+++ common/plugins/login/client/scripts/controllers/login.js
@@ -2,11 +2,29 @@
 /*global angular, $snaphy, $scope, $stateParams */
 
 angular.module($snaphy.getModuleName())
-.controller('loginControl', ['$scope', 'Database',
-    function($scope, Database) {
+.controller('loginControl', ['$scope', 'Database', '$location',
+    function($scope, Database, $location) {
         //Controller defined here..
         $snaphy.template = false;
-        var userService = Database.getDb('login', 'User');
+        var UserService = Database.getDb('login', 'User');
+
+        $scope.credentials = {};
+
+        $scope.login = function(credentials){
+            //Now login to the employee ..
+            UserService.login($scope.credentials, function(values){
+                console.log("Loggenin Redirecting");
+                //Redirect to dashboard otherwise..
+                var redirect = $location.nextAfterLogin || '/';
+                console.log()
+                $location.path(redirect);
+                console.log("Successfully Logged in.");
+            },function(respHeader){
+                //TODO SHOW LOGIN FALIURE ON CONSOLE..
+                console.error("Error getting logged in.");
+            });
+        };
+
         
 
     }//controller function..
