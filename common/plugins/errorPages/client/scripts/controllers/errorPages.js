aa2ed44f47b0ce20a79326640147def8f1c5061f
robinskumar73 <robinskumar73@gmail.com>
2015-12-04 06:04:20 +0530

    Error Pages plugin added

---
+++ common/plugins/errorPages/client/scripts/controllers/errorPages.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module($snaphy.getModuleName())
+
+//Controller for errorPagesControl ..
+.controller('errorPagesControl', ['$scope', '$stateParams', 'Database',
+    function($scope, $stateParams, Database) {
+        //Controller defined here..
+         
+        	$snaphy.setDefaultTemplate(false);
+        
+        //Use Database.getDb(pluginName, PluginDatabaseName) to get the Database Resource.
+    }//controller function..
+]);
---
+++ common/plugins/errorPages/readMe.md
@@ -0,0 +1,12 @@
+# errorPages plugin for Snaphy
+
+
+###A plugin for generating error pages for backend and frontend
+
+###This plugin is exposed on  `/errorPages` route
+
+
+
+
+####Written by Robins Gupta
+
