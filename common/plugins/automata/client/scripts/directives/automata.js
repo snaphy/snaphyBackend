c33d44cecff52fa92b654f6ad02e6620a726d8fa
robinskumar73 <robinskumar73@gmail.com>
2015-12-08 05:13:43 +0530

    html for automata design done

--- common/plugins/automata/client/scripts/directives/automata.js
+++ common/plugins/automata/client/scripts/directives/automata.js
@@ -4,14 +4,29 @@
 angular.module($snaphy.getModuleName())
 
 
-    .directive('snaphyLoadDatatable', ['$timeout', function ($timeout) {
-        return {
-            restrict: 'A',
-            link: function () {
-               $timeout(function(){
-                   // Initialize when page loads
-                   jQuery(function(){ BaseTableDatatables.init(); });
-               }); //timeout method..
-            }//End of Link function...
-        }; // End of return
-    }]);
+.directive('snaphyLoadDatatable', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function () {
+           $timeout(function(){
+               // Initialize when page loads
+               jQuery(function(){ BaseTableDatatables.init(); });
+           }); //timeout method..
+        }//End of Link function...
+    }; // End of return
+}])
+
+.directive('snaphyLoadFilters', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function () {
+
+            $timeout(function(){
+                $(function () {
+                    // Init page helpers (BS Datepicker + BS Colorpicker + Select2 + Masked Input + Tags Inputs plugins)
+                    App.initHelpers(['datepicker',  'select2']);
+                });
+            }); //timeout method..
+        }//End of Link function...
+    }; // End of return
+}]);
