79526aee3e7d02fb6b7aa0933b330e170af7a8e7
robinskumar73 <robinskumar73@gmail.com>
2015-12-13 03:44:09 +0530

    Automata in progress

--- common/plugins/automata/backend/index.js
+++ common/plugins/automata/backend/index.js
@@ -14,8 +14,56 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 	 * @return {[type]} [description]
 	 */
 	var init = function(){
+		//For loading the raw properties..
+		//console.log(server.models.Employee.definition.rawProperties);
+		//For loading the settings..
+		//console.log(server.models.Employee.definition.settings);
+		//Just Introduce a remote method in all the given method..
+		var loadModelList = packageObj["loadModels"];
+		//run each models in the loop and add a remote method to it.
+		loadModelList.forEach(function(modelName, index){
+
+		});
+	};
+
+
+	/**
+	 * Add remote methods to the models..
+	 * @param app
+	 * @param modelname
+	 * @param index
+     */
+	var addRemoteMethod = function(app, modelname, index){
+			var modelObj = app.models[modelname];
+			modelObj.getSchema = function(callback) {
+				//Now form the schema and send it to the client..
+				var modelProperties = modelObj.definition.rawProperties;
+				var relations = model.definition.settings.relations;
+				var filters = model.definition.settings.filters;
+				var tables = model.definition.settings.tables;
+				var hiddenProperties = model.definition.settings.hidden;
+
+				/**
+				 * Now form the desired schema and return it.
+				 */
+				var schema = {};
+
+
+
+				callback(null, schema);
+			};
+
+			//Now registering the method
+			modelObj.remoteMethod(
+					'getSchema',
+					{
+						returns: {arg: 'schema', type: 'object'},
+						description: "Send the schema of the model requested."
+					}
+			);
 
 	};
+
 
 
 	//return all the methods that you wish to provide user to extend this plugin.
