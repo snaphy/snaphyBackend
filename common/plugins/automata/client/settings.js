b9116247f2425616434b11d03117c1cc8617222f
robinskumar73 <robinskumar73@gmail.com>
2015-12-30 21:43:23 +0530

    Database upto recipes done

--- common/plugins/automata/client/settings.js
+++ common/plugins/automata/client/settings.js
@@ -1,14 +1,25 @@
 /**
  * Created by robins on 2/12/15.
  */
-(function(){
+(function() {
     'use strict';
 })();
 /*global jslint browser: true, $snaphy*/
 //This is the setting file of the plugin..TO be configured according to the user needs..
 var settings = {
-   "defaultTemplate": true,
-   loadDatabases:["Employee", "EmployeeDetails", "Recipe", "Comments", "Category", "Customer", "Cuisines", "IngredientCategory", "Ingredients"]
+    "defaultTemplate": true,
+    loadDatabases: [
+        "Employee",
+        "EmployeeDetails",
+        "Recipe",
+        "Comments",
+        "Category",
+        "Customer",
+        "Cuisines",
+        "IngredientCategory",
+        "Ingredients",
+        "Priority"
+    ]
 };
 
 
