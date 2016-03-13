7f2ad5e2c5c77443f115e6cf7c886c52004d2d61
robinskumar73 <robinskumar73@gmail.com>
2016-03-13 18:58:04 +0530

    Adding relational validation

--- common/plugins/JqueryValidate/client/scripts/validateDirectives.js
+++ common/plugins/JqueryValidate/client/scripts/validateDirectives.js
@@ -50,4 +50,4 @@ angular.module($snaphy.getModuleName())
     $validatorProvider.addMethod("valueNotEquals", function(value, element, arg) {
         return arg !== value;
     }, "Please select some value");
-})
+});
