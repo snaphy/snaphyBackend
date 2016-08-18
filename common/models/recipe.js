9ae904add4e40fa1f50cf3b0447aaaa014f59711
robinskumar73 <robinskumar73@gmail.com>
2016-08-18 17:24:24 +0530

    Solving 2days recipe not getting uploaded bugs in progress

--- common/models/recipe.js
+++ common/models/recipe.js
@@ -37,6 +37,8 @@ module.exports = function(Recipe) {
     });
 
     Recipe.beforeRemote("*.*", function(ctx, user, next) {
+        console.log(ctx.req);
+        console.log("\n\n\n\n");
         console.log(ctx.req.body);
         console.log("Access Token: " + ctx.req.accessToken);
         console.log("\n\n\t\t\tUser data", user);
