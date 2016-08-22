55b427bdfb0ec8e6a8f765321e4f7d08654ca768
robinskumar73 <robinskumar73@gmail.com>
2016-08-22 20:46:19 +0530

    Fixing bufs

--- common/models/customer.js
+++ common/models/customer.js
@@ -33,7 +33,7 @@ module.exports = function(Customer) {
 
 
     Customer.beforeRemote("findById", function(ctx, user, next) {
-        console.log(ctx);
+        //console.log(ctx);
         next();
     });
 
